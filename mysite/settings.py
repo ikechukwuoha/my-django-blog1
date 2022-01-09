ea6561eda92149691e26374ccf94f4fe26d5278b
ikechukwu <ikechukwuandrew4@gmail.com>
2022-01-09 19:18:57 +0100

    ikechukwu1234.

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-8o8@j6q6-d0!)dni9@6-a8+@c(=&z0(_4&krxzgriskp=-oju1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
